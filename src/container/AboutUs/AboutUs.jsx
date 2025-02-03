805e5e18b074bee62f89d40e67ba1443c38baf8c
Pavan Vaishnav <pavangvaishnav@gmail.com>
2025-02-03 18:23:14 +0530

    changed the background color of about page

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -16,14 +16,11 @@ const AboutUs = () => (
         <Button className="about-button">Know more</Button>
         
     </div>
-       <div id="div2">
-       
-        {/* <div id='divforknife'> */}
-           <img src="knife.png" className='knife'/>
-        {/* </div> */}
+    <div id="div2">
+        <img src="knife.png" className='knife'/>
         <img src="G.png" id="Gimg" />
-      </div>
-      <div className="about-us-container">
+    </div>
+    <div className="about-us-container">
       <h2 className="about-title">Our History</h2>
       <img src="spoon.png" alt="spoon" className="spoon-img" />
       <p className="about-text about-text2">
@@ -33,6 +30,7 @@ const AboutUs = () => (
       </p>
       <Button className="about-button">Know more</Button>
     </div>
+      
     </div>
   </section>
 );
