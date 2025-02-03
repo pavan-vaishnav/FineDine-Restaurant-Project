066e6b658b0c61002bef810c824582e121e0a79d
Pavan Vaishnav <pavangvaishnav@gmail.com>
2025-02-03 18:15:28 +0530

    changed the margin of header section for better ui experionce

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
       <h5 id="firstline">Chase the new flavour</h5>
       <img src="spoon.png" id='header-spoon'/>
       <h1 id="secondline">THE KEY TO FINE DINING</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eaque corrupti qui eligendi facilis repudiandae illo mollitia placeat, ab alias?</p>
+      <p id='header-ptag'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eaque corrupti qui eligendi facilis repudiandae illo mollitia placeat, ab alias?</p>
       <Button variant="light" id="explorebtn">Explore More</Button>
     </div>
     {/*  */}
